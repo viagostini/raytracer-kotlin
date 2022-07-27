f37d0c9a4d247273bfa0e8498a770fa5928a1227
Vinicius Agostini <vinicius.agostini19@gmail.com>
2022-07-27 09:58:56 -0300

    Project structure with Kotest dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.4.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
